290497d8b1d6b7702c2d014cad6a84c9263f5702
Jonarod <jonrodd@gmail.com>
2020-11-27 01:50:49 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Install
+
+```
+npm i --save capacitor-navigation-bar
+npx cap sync
+```
+
+
+# Usage
+
+```
+import { NavigationBar } from Plugins;
+
+...
+
+var options = {
+  bgColor: "#FFFFFF",
+  theme: "light", // or "dark"
+  transparent: false,
+  hide: false
+}
+
+NavigationBar.set(options);
+```
+
+# Work In Progress
+
+`transparent` and `hide` options won't work for the moment... this is work in progress. I am open to PRs ;)
+
--- android/src/main/java/com/jonarod/capacitor/navigationbar/NavigationBar.java
+++ android/src/main/java/com/jonarod/capacitor/navigationbar/NavigationBar.java
@@ -1,5 +1,11 @@
 package com.jonarod.capacitor.navigationbar;
 
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.Window;
+import android.view.View;
+import android.view.WindowManager;
+
 import com.getcapacitor.JSObject;
 import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
@@ -9,12 +15,50 @@ import com.getcapacitor.PluginMethod;
 @NativePlugin
 public class NavigationBar extends Plugin {
 
-    @PluginMethod
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
+    @PluginMethod()
+    public void set(PluginCall call) {
 
-        JSObject ret = new JSObject();
-        ret.put("value", value);
-        call.success(ret);
+        /** @TODO:
+        * get hex color string FOR THE BACKGROUND from the bridge using:
+        *   call.getString("bgColor");
+        */
+        String bgColor = call.getString("bgColor");
+
+        if(bgColor == null || bgColor.equals("") || !bgColor.contains("#")){
+            call.error("Backgroud color cannot be empty or null. It should be a hex value, eg: #d1009d");
+        }
+
+
+        /** @TODO:
+        * Set android navigation bar background and buttons colors
+        * or send error signal to the bridge using: call.error('<some_error_message>');
+        */
+        try{
+            final int parsedBgColor = Color.parseColor(bgColor);
+            final Window window = ((Activity) getContext()).getWindow();
+
+            // only update views on ui thread
+             getActivity().runOnUiThread(new Runnable() {
+                 @Override
+                 public void run() {
+                     window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                     window.setNavigationBarColor(parsedBgColor);
+                     window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR | View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR);
+                 }
+             });
+
+
+        } catch (Exception e){
+            call.error(e.toString());
+        }
+
+
+        /** @TODO:
+        * if everything ok: send success signal to the bridge using: 
+        *   call.success('<some_success_message>');
+        */
+        call.success();
+
     }
+
 }
